18d6cc45901737c859ccc8ca3ecd82962a5cddec
tamatebox <ryota.ido.kyoto@gmail.com>
2020-05-17 18:01:17 +0900

    shell

---
+++ shell_sort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+using namespace std;
+static const int MAX = 1000000;
+
+long long cnt;
+int l;
+int A[MAX];
+int n;
+vector<int> G;
